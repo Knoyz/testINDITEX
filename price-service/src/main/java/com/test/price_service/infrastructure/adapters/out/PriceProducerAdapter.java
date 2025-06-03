5f67ebe1e4825144c6431a03cf427881df366a5f
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 <k.noy@cesc-it.com>
2025-06-03 11:01:36 +0200

    Implement Kafka integration in PriceProducerAdapter for sending price update events

--- price-service/src/main/java/com/test/price_service/infrastructure/adapters/out/PriceProducerAdapter.java
+++ price-service/src/main/java/com/test/price_service/infrastructure/adapters/out/PriceProducerAdapter.java
@@ -2,13 +2,20 @@ package com.test.price_service.infrastructure.adapters.out;
 
 import com.test.price_service.domain.events.PriceUpdateEvent;
 import com.test.price_service.domain.ports.out.PriceEventProducerPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class PriceProducerAdapter implements PriceEventProducerPort {
+
+    private final KafkaTemplate<String, PriceUpdateEvent> kafkaTemplate;
+
+    private static final String TOPIC = "updated-price-event";
 
     @Override
     public void sendPriceUpdateEvent(PriceUpdateEvent event) {
-
+        kafkaTemplate.send(TOPIC, event);
     }
 }
